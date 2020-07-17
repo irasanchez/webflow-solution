a758986534976730c06cf674ad9650dfbb97604d
Ira <ira.d.sanchez@gmail.com>
2020-07-16 18:47:12 -0700

    Add Apollo client

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({ uri: "https://graphqlzero.almansi.me/api" });
+
+const ApolloApp = (AppComponent) => (
+  <ApolloProvider client={client}>
+    <AppComponent />
+  </ApolloProvider>
 );
+
+ReactDOM.render(ApolloApp(App), document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
