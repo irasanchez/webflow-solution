edc1228e0a97b756740d6a3bcc37a8666de33665
Ira <ira.d.sanchez@gmail.com>
2020-07-19 03:09:36 -0700

    switch graphql libraries
    - I only needed request abilities

--- app/src/index.js
+++ app/src/index.js
@@ -3,18 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 
-const client = new ApolloClient({ uri: "https://rickandmortyapi.com/graphql" });
-
-const ApolloApp = (AppComponent) => (
-  <ApolloProvider client={client}>
-    <AppComponent />
-  </ApolloProvider>
-);
-
-ReactDOM.render(ApolloApp(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
